2cba254efb99cd237d86f03835ea13f86a7eece4
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2016-04-22 11:15:23 +0200

    Added routine to evaluate interpolation on a non-linear grid

--- src/InterpNd.jl
+++ src/InterpNd.jl
@@ -1,5 +1,21 @@
 module InterpNd
 
-# package code goes here
+using Interpolations
+
+import Base.call
+function call(itp::Interpolations.BSplineInterpolation,
+              x::AbstractVector,
+              xx::AbstractVector)
+    didx = [0; 1.0./diff(x)]
+    is = map(eachindex(xx)) do i
+        im = indmin(abs(x-xx[i]))
+        im - didx[im]*(x[im] - xx[i])
+    end
+    Float64[itp[ii] for ii in is]
+end
+
+interp1d = y -> interpolate(y, BSpline(Linear()), OnGrid())
+
+export call, interp1d
 
 end # module
